30b211360a6b0822bc9f0cc178d62b3cd7e4cf3a
Darmen Muratbekov <75259056+darmenmuratbekov@users.noreply.github.com>
2022-01-13 17:30:39 +0600

    Update h.cpp

--- Final2020_g1/h.cpp
+++ Final2020_g1/h.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector> 
+#include <string> 
 
 
 using namespace std; 
@@ -23,40 +24,57 @@ int main() {
             b = b + b[i];
         }
     }
+    b = b.substr(0, a.size());
     int xa,xb;
     int xab;
+    char p = 't' + 1;
+    char t = 'p'; 
     char newxb;
     char newxa;
-    char newab;
-    int newabint = 'a';
+    int newabint = 97;
     int changeint;
+    char newab;
     int newabintresult;
+    int xabs;
+    char abresult = 'a' + 34;
     for(int i = 0; i < a.size(); i++) {
          xa = a[i] - 'a'; // 'b', 1 = 'b' - 'a' 
          xb = b[i] - 'a'; // 'c', 2 = 'c' - 'a' 
                           // xa = a[i] 'e', 4  = 'e' - 'a'
                           // xb = b[i]  'b', 1 = 'b' - 'a'
-         xab = xa + xb;
-         newab = 'a' + xab;
-         if('z' >= newab) {
+         xab = xa + xb; // if
 
-            cout << newab;
+         if(26 > xab) { // 25 24 23 22 21
+
+             newab  = 'a' + xab;
+             cout << newab;
+
+         }
+         if(xab  >= 26) { // 26 27 28 29
+
+             xab = xab % 26; 
+             newab = xab + 'a';
+             cout << newab;
+
+         }
        //     changeint = newab;
         //    cout << changeint << endl;
 
-         }
-         if(newab  > 'z') {
-
-            newabintresult = newabint + xab;
-            cout << newabintresult << endl; 
-          //  cout << newabintresult;
-            newabintresult = newabintresult - 'z' - 1;
-          //  cout << newabintresult <<" ";
-            newab = newabintresult + 'a';
-            cout << newab;
-
-         }
-
+         
     }
+    // it's second solution  of this problems
+         return 0;
+         string ans = "";
+         char kl;
+         for (int i = 0; i <  b.size(); i++) {
+             xa = a[i] - 'a';
+             xb = b[i] - 'a';
+             cout << xa  <<  " " << xb << endl;
+             
+             kl=  ((xa + xb) % 26) + 97;
+             ans  = ans  + kl;
+         }
+          cout << ans << endl;
+    
     return 0;
 }
